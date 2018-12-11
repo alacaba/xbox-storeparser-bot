9887a5a6752b0df35dfa746e4cd9553af84c537e
alacaba <alacaba@fastmail.com>
2018-12-11 17:39:50 +0800

    fetch and display deals

--- Gemfile
+++ Gemfile
@@ -6,3 +6,6 @@ source 'https://rubygems.org'
 ruby '2.5.3', :patchlevel => '105'
 
 gem 'telegram-bot-ruby'
+gem 'xbox-storeparser', :git => 'git@github.com:alacaba/xbox-storeparser.git'
+gem 'nokogiri'
+gem 'dotenv'
--- bot.rb
+++ bot.rb
@@ -1,2 +1,30 @@
 require 'telegram/bot'
-p 'it works'
+require 'xbox/storeparser'
+require 'dotenv'
+
+Dotenv.load
+
+token = ENV['TELEGRAM_BOT_TOKEN']
+
+CHUNK_SIZE = 20.freeze
+deals      = Xbox::Storeparser::Deals.new(locale: 'en-US')
+my_deals   = deals.fetch
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |msg|
+    _, *params = msg.text.split
+
+    if msg.text.include?('/deals')
+      fmt_deals = my_deals.map(&:to_s)
+
+      (0..fmt_deals.length).step(CHUNK_SIZE).each do |chunk|
+        first, last = chunk, (chunk+CHUNK_SIZE)
+        deals = (first..last).map { |i| fmt_deals[i] }.join("\n")
+        bot.api.send_message(
+          chat_id: msg.chat.id,
+          text: deals,
+          parse_mode: "markdown")
+      end
+    end
+  end
+end
